42aa2a84953d943061ac4b911cac72dc2d19c265
sowmya-pusapati <pusapatisowmya@gmail.com>
2023-01-12 23:52:41 +0530

    removed printing statements

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,12 +3,10 @@ package com.driver;
 public class Main {
     public static void main(String[] args){
         Product p=new Product();
-      int a=  p.product(3,4);
-      int b=  p.product(3,4,5);
-       double d= p.product(3.45,5.6);
-       System.out.println(a);
-        System.out.println(b);
-        System.out.println(d);
+        p.product(3,4);
+        p.product(3,4,5);
+        p.product(3.45,5.6);
+
 
     }
 
