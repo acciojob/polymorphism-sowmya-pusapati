8d53c1fd5c0cec900a98ea3973b0327a71cb1666
sowmya-pusapati <pusapatisowmya@gmail.com>
2023-01-13 13:01:04 +0530

    changing the code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,27 +1,28 @@
 package com.driver;
 
 public class Main {
+
     public static void main(String[] args){
         Product p=new Product();
-        p.product(3,4);
-        p.product(3,4,5);
-        p.product(3.45,5.6);
+        p.product(5,6);
+        p.product(5,6,7);
+        p.product(2.4,5.6);
 
 
     }
-
-}
-class Product{
+    public static class Product{
 
 
-    public int product(int x, int y){
-        return x*y;
-    }
-    public int product(int x,int y,int z){
-        return (int)x*y*z;
-    }
-    public double product(double x,double y){
-        return x*y;
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return (x*y*z);
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+
     }
 
 }
